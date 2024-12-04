d240a33ca54ca29690b61154bafd918be3d64ae9
kristophernorth <kristopher.north@gmail.com>
2024-12-04 16:59:41 -0700

    saving in appState

--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { AppState } from "@/AppState.js"
+import { House } from "@/models/House.js"
 
 class HousesService {
   async getHouses() {
     const response = await api.get('api/houses')
     logger.log('Got houses', response.data)
+    const houses = response.data.map(housePOJO => new House(housePOJO))
+    AppState.houses = houses
   }
 }
 
