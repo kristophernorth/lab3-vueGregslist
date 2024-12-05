bcd8bdb5323ed631dc00123281b1a37fa15d6402
kristophernorth <kristopher.north@gmail.com>
2024-12-04 19:23:11 -0700

    stopping for the night

--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { AppState } from '@/AppState';
+import HouseForm from '@/components/globals/HouseForm.vue';
 import HouseListing from '@/components/HouseListing.vue';
 import { housesService } from '@/services/HousesService';
 import { logger } from '@/utils/Logger';
@@ -30,6 +31,12 @@ async function getHouses() {
     <section class="row mb-3">
       <div class="col-12">
         <h1 class="text-center">Houses</h1>
+      </div>
+    </section>
+
+    <section class="row">
+      <div class="col-12">
+        <HouseForm />
       </div>
     </section>
 
